c847d0e80fa3b40f1dd28f69fd4201d67ea2360f
Admin <307474304@qq.com>
2018-08-23 22:31:24 +0800

    添加了一个实体类

---
+++ src/main/java/com/qf/demo/pojo/po/Student.java
@@ -0,0 +1,16 @@
+package com.qf.demo.pojo.po;
+
+/**
+ * Created by Admin on 2018/8/23.
+ */
+public class Student {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
